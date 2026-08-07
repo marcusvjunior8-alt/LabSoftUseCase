54496925b98da48a7291ba6019b510de30abdf2b
Marcus <marcus.v.junior8@aluno.senai.br>
2026-08-07 16:20:57 -0300

    fwef

--- SLN_Dias/AppDias/Program.cs
+++ SLN_Dias/AppDias/Program.cs
@@ -7,7 +7,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("=================================");
-        Console.WriteLine(" SoftwareHouse - SISTEMA DE GESTÃO DE TAREFAS");
+        Console.WriteLine(" SoftwareHouse - SISTEMA DE GESTÃO DE TAREFA");
         Console.WriteLine("=================================\n");
 
         Console.Write("Digite o nome da tarefa: ");
